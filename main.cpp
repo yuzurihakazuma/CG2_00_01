cdc87897ff9bb36987d9b7c015882e975b59d323
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-27 11:19:04 +0900

    a

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #include "externals/imgui.h"
 #include "externals/imgui_impl_dx12.h"
 #include "externals/imgui_impl_win32.h"
-#include "externals/DirectXTex/DirectXTex.h"
+
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
