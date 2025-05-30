f26ed07fddef4f3a75a429627678d2d454fb42f3
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-30 13:20:09 +0900

    こっちが本命

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,25 +1,25 @@
-struct TransformationMatirx{
+#include "object3d.hlsli"
+struct TransformationMatirx
+{
     float32_t4x4 WVP;
 };
 ConstantBuffer<TransformationMatirx> gTransformationMatrix : register(b0);
 
-// 頂点シェーダーの出力
-struct VertexShaderOutput{
-    // (クリッピング空間での座標)
-    float32_t4 position : SV_POSITION;
-    
-};
+
 // 頂点シェーダーの入力
 struct VertexShaderInput
 {
     // 変換済みの座標(スクリーン空間)
     float32_t4 position : POSITION0;
+    float32_t2 texcoord : TEXCOORD0;
     
 };
 // 頂点シェーダー本体
-VertexShaderOutput main(VertexShaderInput input){
+VertexShaderOutput main(VertexShaderInput input)
+{
     VertexShaderOutput output;
     output.position = mul(input.position, gTransformationMatrix.WVP);
+    output.texcoord = input.texcoord;
     return output;
 }
 
