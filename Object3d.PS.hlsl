d14d7e543057324942b2cec7d4cc7c56458a3be7
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-30 13:14:30 +0900

    あ

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -15,4 +15,26 @@ PixelShaderOutput main()
     output.color = gMaterial.color;
     return output;
     
+}#include "object3d.hlsli"
+
+struct Material
+{
+    float32_t4 color;
+};
+ConstantBuffer<Material> gMaterial : register(b0);
+Texture2D<float32_t4> gTextrue : register(t0);
+SamplerState gSampler : register(s0);
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    float32_t4 textrueColor = gTextrue.Sample(gSampler, input.texcoord);
+    output.color = gMaterial.color * textrueColor;
+    return output;
+    
 }
