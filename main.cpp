171cd887534e1ff423b9b45b8957a7cad2d70261
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-06-25 16:49:11 +0900

    w

--- main.cpp
+++ main.cpp
@@ -1377,6 +1377,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			ImGui::SliderFloat("LightY", &directionalLightData->direction.y,-10.0f,10.0f);
 			ImGui::SliderFloat("LightZ", &directionalLightData->direction.z,-10.0f,10.0f);
 
+			directionalLightData->direction = Normalize(directionalLightData->direction);
 
 			ImGui::End();
 
