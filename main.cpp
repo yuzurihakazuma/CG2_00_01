42954ffb3ad1e170a3842edce0075055da78dbf0
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-07-29 10:24:21 +0900

    あった

--- main.cpp
+++ main.cpp
@@ -130,7 +130,6 @@ struct D3DResourceLeakChecker{
 			debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
 			debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
 			debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-			debug->Release();
 		}
 	}
 
@@ -1998,11 +1997,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int){
 	SoundUnload(&soundData1);
 	
 
-#ifdef _DEBUG
 
-	debugController->Release();
-
-#endif // _DEBUG
 	CloseWindow(hwnd);
 
 
