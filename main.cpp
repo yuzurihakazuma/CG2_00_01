3781281d953b1ccf27a264841857b9c6108eae8e
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-07-29 10:22:24 +0900

    ｗ

--- main.cpp
+++ main.cpp
@@ -126,7 +126,7 @@ struct D3DResourceLeakChecker{
 			debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
 			debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
 			debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-			debug->Release();
+			
 		}
 	}
 
