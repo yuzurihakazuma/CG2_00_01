25695d8b7e8d7fec102d612e03869dee3eae7cb4
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-10-06 16:18:33 +0900

    修正した

--- project/engine/3d/DebugCamera.h
+++ project/engine/3d/DebugCamera.h
@@ -1,50 +1,50 @@
 //#pragma once
 //
 //#include"struct.h"
-//#include"Matrix4x4.h"
-//#include <Windows.h>
+//#include"matrix4x4.h"
+//#include <windows.h>
 //#include <cmath>
 //// デバックカメラのクラス
 //
-//using namespace MatrixMath;
+//using namespace matrixmath;
 //
-//class DebugCamera{
+//class debugcamera{
 //public:
 //
 //
 //
 //	// 初期化
-//	void Initialize();
+//	void initialize();
 //	// 更新
-//	void Update();
+//	void update();
 //	// 描画
-//	void Draw();
+//	void draw();
 //
-//	void MouseMove(float dx, float dy);
+//	void mousemove(float dx, float dy);
 //
-//	const Matrix4x4& GetViewMatrix() const{return viewMatrix_;}
+//	const matrix4x4& getviewmatrix() const{return viewmatrix_;}
 //	
-//	bool IsActive() const{ return isDebugCamera_; } // 追加
+//	bool isactive() const{ return isdebugcamera_; } // 追加
 //	
 //private:
 //
 //	// カメラのローカル回転
-//	Vector3 rotation_;
+//	vector3 rotation_;
 //
 //	//カメラの位置
-//	Vector3 translation_;
+//	vector3 translation_;
 //	// ビュー行列
-//	Matrix4x4 viewMatrix_ = MakeIdentity4x4();
+//	matrix4x4 viewmatrix_ = makeidentity4x4();
 //	// 射影行列
-//	Matrix4x4 projectionMatrix_ = MakeIdentity4x4();
+//	matrix4x4 projectionmatrix_ = makeidentity4x4();
 //
 //	// ビュー行列の更新
-//	void UpdateViewMatrix();
+//	void updateviewmatrix();
 //	// 入力に夜カメラの移動や回転を行う
-//	void InputUpdate();
+//	void inputupdate();
 //
-//	bool isDebugCamera_ = false; // デフォルトは通常カメラ
-//	bool prevToggleKey_ = false; // トグル用前フレームキー
+//	bool isdebugcamera_ = false; // デフォルトは通常カメラ
+//	bool prevtogglekey_ = false; // トグル用前フレームキー
 //
 //};
 //
